776ab264e11cfb995f18a8cd0ba882485ed27903
HornSina <166434033+HornSina@users.noreply.github.com>
2024-04-09 00:56:27 +0700

    Add files via upload

---
+++ Ho.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int number1;
+	int number2;
+	int total;
+	
+	cout<<"Calculator"<<endl;
+	cout<<"Please enter 2 number to calculator;"<<endl;
+	cout<<"Fist Number=";
+	cin>>number1;
+	cout<<"second Number=";
+	cin>>number2;
+	cout<<"="<<number1 + number2<<endl;
+	cout<<"="<<number1 - number2<<endl;
+	cout<<"="<<number1 * number2<<endl;
+	cout<<"="<<number1 / number2<<endl;
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
